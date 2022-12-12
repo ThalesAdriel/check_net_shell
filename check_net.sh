1d079f8f67a90e0e9cc1ed124daa0011e8abd299
ThalesAdriel <115805292+ThalesAdriel@users.noreply.github.com>
2022-12-12 18:42:04 +0000

    Update check_net.sh

--- check_net.sh
+++ check_net.sh
@@ -11,10 +11,10 @@ conection_status(){
 	Ping1=$(ping -c 2 8.8.8.8 > /dev/null);
 	if [ $? -eq 0 ];then
 	echo -e "${Color_green} |Conectado| ${Color_default}"
-	mpg123 '/media/cerberus/AAA/shell/check_net_sound/Accept.mp3'
+	mpg123 '~/check_net_sound/Accept.mp3'
 	else
 	echo -e "${Color_red} |Desconectado| ${Color_default}"
-	mpg123 '/media/cerberus/AAA/shell/check_net_sound/failed.mp3'
+	mpg123 '~/check_net_sound/failed.mp3'
 	fi
 }
 conection_status
