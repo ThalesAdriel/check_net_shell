5d79a3423257b0d8a2c1a6e54fb6f995384266f0
ThalesAdriel <115805292+ThalesAdriel@users.noreply.github.com>
2022-12-12 18:37:00 +0000

    Add files via upload

---
+++ check_net.sh
@@ -0,0 +1,22 @@
+#1/bin/bash
+
+Color_green="\033[1;92m"
+Color_default="\033[0;0m"
+Color_red="\033[1;31m"
+
+while true
+do
+conection_status(){
+	echo "Conectividade com internet..."
+	Ping1=$(ping -c 2 8.8.8.8 > /dev/null);
+	if [ $? -eq 0 ];then
+	echo -e "${Color_green} |Conectado| ${Color_default}"
+	mpg123 '/media/cerberus/AAA/shell prova/questão_2_sound/Accept.mp3'
+	else
+	echo -e "${Color_red} |Desconectado| ${Color_default}"
+	mpg123 '/media/cerberus/AAA/shell prova/questão_2_sound/failed.mp3'
+	fi
+}
+conection_status
+sleep 30
+done
